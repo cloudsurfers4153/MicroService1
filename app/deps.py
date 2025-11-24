7316dfdec4ed64c02352a1b745a47fa12ead05fd
wd2002 <wangdi_wd2002@163.com>
2025-11-23 21:38:18 -0500

    Update version

--- app/deps.py
+++ app/deps.py
@@ -1,19 +1,12 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from app.database import SessionLocal
+from app.database import get_db
 from app import crud
 from app.auth import decode_access_token
 from app.schemas import TokenPayload
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/sessions")
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
--- app/schemas.py
+++ app/schemas.py
@@ -18,7 +18,7 @@ class UserOut(BaseModel):
     updated_at: datetime
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class Token(BaseModel):
     access_token: str
